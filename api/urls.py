e13b5fa5fbaf3466ea9dad9ed282d8528a64838c
cement-hools <cement-fan@ya.ru>
2021-09-12 22:01:50 +0300

    functions finished

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import visited_links
+from .views import visited_links, visited_domains
 
 urlpatterns = [
-    path('', visited_links, name='visited_links'),
-    path('', visited_links, name='visited_links'),
+    path('visited_links/', visited_links, name='visited_links'),
+    path('visited_domains/', visited_domains, name='visited_domains'),
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
